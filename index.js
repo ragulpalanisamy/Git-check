b249d209bd6bfc82d82e5a114a9bf26e71e868da
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-04-29 10:03:57 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Ragul")
+const axios = require("axios");
+const { get } = require("lodash");
+
+const callApi = async () => {
+  const url = "https://reqres.in/api/movie";
+  try {
+    const response = await axios.get(url);
+    console.log(get(response, "data", "Not Found"));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+callApi();
