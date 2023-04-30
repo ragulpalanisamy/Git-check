6291f74670f240cb4667ce7faaddf0878a78729c
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-04-30 16:16:42 +0530

    initial commit

--- express.js
+++ express.js
@@ -100,3 +100,13 @@ app.listen(port, () => {
 
 
  */
+const express = require("express");
+const router = require("./routes");
+
+const app = express();
+
+app.use(express.json());
+app.use("/api",router);
+app.listen(8000,()=>{
+  console.log("Server is up and Running")
+})
